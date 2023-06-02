0f19a39a9030f95e151299219bdb6880fa018460
Andrewfpai <nicolasandrew.28@gmail.com>
2023-06-02 18:11:37 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+tailwind.config = {
+  theme: {
+    fontSize: {
+      paragraph: '16px',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.5rem',
+      '5xl': '2.75rem',
+      '6xl': '3.75rem',
+      '7xl': '4.5rem',
+    },
+
+    fontFamily: {
+      publicSans : "'Public Sans', sans-serif"
+    },
+
+    extend: {
+      colors: {
+  
+        darkBlue: "#2d314d",
+        limeGreen: "#31d35c",
+        brightCyan: "#2bb7da",
+
+        grayishBlue: "#9698a6",
+        lightGrayishBlue: "#f3f4f6",
+        veryLightGray: "#fafafa"
+      }
+    }
+  }
+}
